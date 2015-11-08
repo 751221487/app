19683d0825a76488e3367bae794aaa167a95ae30
HaoJie <haojie@HaoJiedeMacBook-Air.local>
2015-11-09 00:39:34 +0800

    delete runtime files

--- Public/Runtime/Temp/4a59346e6a174a74fb0e38d4a4d4c4d0.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000000a:1:{i:0;a:3:{s:2:"id";i:0;s:4:"text";s:6:"全国";s:8:"children";a:1:{i:0;a:3:{s:2:"id";s:1:"4";s:4:"text";s:9:"江苏省";s:8:"children";a:1:{i:0;a:3:{s:2:"id";s:1:"5";s:4:"text";s:9:"南京市";s:8:"children";a:2:{i:0;a:3:{s:2:"id";s:1:"7";s:4:"text";s:9:"秦淮区";s:8:"children";a:1:{i:0;a:3:{s:2:"id";s:1:"8";s:4:"text";s:3:"asd";s:8:"children";a:0:{}}}}i:1;a:3:{s:2:"id";s:1:"6";s:4:"text";s:9:"玄武区";s:8:"children";a:0:{}}}}}}}}}
-?>
